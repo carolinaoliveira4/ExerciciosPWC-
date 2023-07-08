0a0183e1af0aa4423fac5f0fd9fd35b736dfd4a6
Maria Carolina Oliveira <mariacarolina.oliveira204@gmail.com>
2023-07-08 16:14:35 -0300

    Update exercicio3.c
    Escrevendo o que a função vai retornar.

--- exercicio3.c
+++ exercicio3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int palindromo(char * frase, int esq, int dir){
-    
+int palindromo(char * str, int esq, int dir){
+     int len = strlen(str);
+    while (esq >= 0 && esq < len && str[esq] == str[dir]) {
+        dir++;
+        esq--;
 }
+    return dir - esq - 1;
+    
+} 
