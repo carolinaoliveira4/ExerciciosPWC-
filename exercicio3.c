991c486e632dca7a296a6292e1ae1c361362b4f2
Maria Carolina Oliveira <mariacarolina.oliveira204@gmail.com>
2023-07-08 16:16:41 -0300

    Update exercicio3.c
    Criando mais uma função.

--- exercicio3.c
+++ exercicio3.c
@@ -8,5 +8,20 @@ int palindromo(char * str, int esq, int dir){
         esq--;
 }
     return dir - esq - 1;
-    
 } 
+
+void substring(char* str) {
+    int len = strlen(str);
+    int inicio = 0, fim = 0;
+
+    for (int i = 0; i < len; i++) {
+        int len1 = palindromo(str, i, i);
+        int len2 = palindromo(str, i, i + 1);
+        int maxLen = (len1 > len2) ? len1 : len2;
+
+        if (maxLen > fim - inicio) {
+            inicio = i - (maxLen - 1) / 2;
+            fim = i + maxLen / 2;
+        }
+    }
+    printf()
